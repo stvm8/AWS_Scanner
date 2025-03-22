0f9d092085e40daa336c116021b602cad080da9b
stvm8 <121763544+stvm8@users.noreply.github.com>
2025-03-22 11:09:03 -0500

    2nd upload

--- __init__.py
+++ __init__.py
@@ -1,8 +1,3 @@
 """
-Core functionality for AWS Permission Toolkit.
+Configuration module for AWS Permission Toolkit.
 """
-
-# Import core modules for easier access
-from .permission_enum import enumerate_permissions, save_results
-from .further_enum import ask_for_further_enumeration
-from .policy_reader import process_policies
